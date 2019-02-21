8d8f06601a0b9df870c758fc1fd4e08c5e09bad0
Roman East-Oz <surdukov@gmail.com>
2019-02-21 19:03:01 +0300

    prepare project

---
+++ bulk.h
@@ -0,0 +1,231 @@
+#ifndef BULK_H
+#define BULK_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <memory>
+#include <time.h>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream> 
+
+class Observer
+{
+public:
+	virtual void execute( std::vector<std::string>*, time_t* ) = 0;
+	virtual ~Observer() = default;
+};
+
+class Executor
+{
+public:
+	Executor(): m_fct( time( 0 ) ) {};
+
+	std::vector<std::string> m_commands;
+	time_t m_fct; // first command time
+private:
+	std::vector<std::shared_ptr<Observer>> m_subscribers;
+
+public:
+	void subscribe( std::shared_ptr<Observer> ptrObs )
+	{
+		m_subscribers.push_back( ptrObs );
+	}
+
+	void set_commands( std::vector<std::string> commands, time_t fct )
+	{
+		m_commands = commands;
+		m_fct = fct;
+		execute();
+	}
+
+	void execute()
+	{
+		for( auto s : m_subscribers )
+		{
+			s->execute( &m_commands, &m_fct );
+		}
+	}
+
+};
+
+class FileObserver: public Observer
+{
+public:
+	FileObserver( std::shared_ptr<Executor> ptrExecutor )
+	{
+		auto wptr = std::shared_ptr<FileObserver>( this, []( FileObserver* ) {} );
+		ptrExecutor->subscribe( wptr );
+	}
+
+	void execute( std::vector<std::string>* commands, time_t* fct ) override
+	{
+		if( commands->size() > 0 )
+		{
+			struct tm  tstruct;
+			char       buf[ 80 ];
+			tstruct = *localtime( fct );
+			strftime( buf, sizeof( buf ), "%OH%OM%OS", &tstruct );
+
+			auto time_now = std::chrono::system_clock::now();
+			auto ms = std::chrono::duration_cast< std::chrono::milliseconds >(time_now.time_since_epoch()) % 1000;
+
+			std::string fn = "bulk";
+			fn.append( buf );
+			fn.append( std::to_string( ms.count() ) );
+			fn.append( ".log" );
+
+			std::ofstream myfile;
+			myfile.open( fn );
+			if( myfile.is_open() )
+			{
+				for( size_t i = 0; i < commands->size(); ++i )
+				{
+					myfile << commands->at( i );
+					if( i != (commands->size() - 1) )
+						myfile << ", ";
+					else
+						myfile << std::endl;
+				}
+				myfile.close();
+			}
+		}
+
+	}
+};
+
+class ConsoleObserver: public Observer
+{
+public:
+	ConsoleObserver( std::shared_ptr<Executor> ptrExecutor )
+	{
+		auto wptr = std::shared_ptr<ConsoleObserver>( this, []( ConsoleObserver* ) {} );
+		ptrExecutor->subscribe( wptr );
+	}
+
+	void execute( std::vector<std::string>* commands, time_t* ) override
+	{
+		if( commands->size() > 0 )
+		{
+			std::cout << "bulk: ";
+			for( size_t i = 0; i < commands->size(); ++i )
+			{
+				std::cout << commands->at( i );
+				if( i < (commands->size() - 1) )
+					std::cout << ", ";
+				else
+					std::cout << std::endl;
+			}
+		}
+	}
+};
+
+class Parser
+{
+public:
+	Parser( std::shared_ptr<Executor> ptrExec, size_t row_count = 3 ):
+		m_pExecutor( ptrExec ), m_nRowCount( row_count ), m_nCount( 1 ), m_nOpenBraces( 0 )
+	{};
+
+	void ParseStringData( std::string data_str )
+	{
+		std::istringstream stream( data_str );
+
+		bool is_ready_data = false;
+
+		m_nCount = m_Commands.size();
+		time_t fct = time( 0 );
+		std::string line;
+
+		for( std::string line; std::getline( stream, line );)
+		{
+			if( line.empty() )
+			{
+				continue;
+			}
+			m_nCount++;
+			if( m_nCount == 1 )
+			{
+				fct = time( 0 );
+			}
+			if( line.find( '{' ) != std::string::npos )
+			{
+				++m_nOpenBraces;
+				if( m_nOpenBraces == 1 )
+				{
+					is_ready_data = true;
+				}
+			}
+			else if( (line.find( '}' ) != std::string::npos) && (m_nOpenBraces > 0) )
+			{
+				--m_nOpenBraces;
+				if( m_nOpenBraces == 0 )
+				{
+					is_ready_data = true;
+				}
+			}
+			else if( (m_nCount == m_nRowCount) && (m_nOpenBraces == 0) )
+			{
+				m_Commands.push_back( line );
+				is_ready_data = true;
+			}
+			else
+			{
+				m_Commands.push_back( line );
+			}
+
+			if( is_ready_data )
+			{
+				m_pExecutor->set_commands( m_Commands, fct );
+				m_Commands.clear();
+				is_ready_data = false;
+				m_nCount = 0;
+			}
+
+		}
+		m_pExecutor->set_commands( m_Commands, fct );
+		m_Commands.clear();
+		is_ready_data = false;
+		m_nCount = 0;
+	}
+
+private:
+	std::shared_ptr<Executor> m_pExecutor;
+	size_t m_nRowCount;
+	std::vector<std::string> m_Commands;
+	size_t m_nCount;
+	size_t m_nOpenBraces;
+};
+
+class ParserWrapper
+{
+public:
+	ParserWrapper( size_t bulk_size )
+	{
+		std::shared_ptr<Executor> ptrExec( std::make_shared<Executor>() );
+
+		m_ptrConsoleObserver = std::shared_ptr<ConsoleObserver>( new ConsoleObserver( ptrExec ) );
+		m_ptrFileObserver = std::shared_ptr<FileObserver>( new FileObserver( ptrExec ) );
+
+		m_ptrParserWorker = std::shared_ptr<Parser>( new Parser( ptrExec, bulk_size ) );
+	}
+	~ParserWrapper() = default;
+
+
+	void ReceiveData( const char *data, std::size_t size )
+	{
+		std::string data_string( data, size );
+		m_ptrParserWorker->ParseStringData( data_string );
+	}
+
+private:
+	std::shared_ptr<FileObserver> m_ptrFileObserver;
+	std::shared_ptr<ConsoleObserver> m_ptrConsoleObserver;
+	std::shared_ptr<Parser> m_ptrParserWorker;
+};
+
+#endif // BULK_H
+
---
+++ bulk_server.cpp
@@ -0,0 +1,132 @@
+// bulk_server.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <string>
+#include "bulk.h"
+
+using namespace boost::asio;
+using namespace boost::posix_time;
+io_service service;
+
+class ClientConnection;
+typedef boost::shared_ptr<ClientConnection> client_ptr;
+typedef std::vector<client_ptr> array;
+array clients;
+
+#ifdef _DEBUG
+int gClient = 1;
+#endif
+
+int gPort = 9000;
+int gBulkSize = 3;
+
+boost::shared_ptr<ParserWrapper> gParserPtr;
+
+class ClientConnection: public boost::enable_shared_from_this<ClientConnection>
+	, boost::noncopyable
+{
+	typedef ClientConnection self_type;
+	ClientConnection(): socket( service ), _started( false )
+	{
+#ifdef _DEBUG
+		_client_id = gClient;
+		++gClient;
+#endif
+	}
+public:
+	typedef boost::system::error_code error_code;
+	typedef boost::shared_ptr<ClientConnection> ptr;
+
+	void Start()
+	{
+		_started = true;
+		clients.push_back( shared_from_this() );
+		Read();
+	}
+	static ptr Create()
+	{
+		ptr new_( new ClientConnection );
+		return new_;
+	}
+	void Stop()
+	{
+		if( !_started ) return;
+		_started = false;
+		socket.close();
+
+		ptr self = shared_from_this();
+		array::iterator it = std::find( clients.begin(), clients.end(), self );
+		clients.erase( it );
+	}
+	bool Started() const { return _started; }
+	ip::tcp::socket & sock() { return socket; }
+
+private:
+	void OnRead( const error_code& err, size_t size )
+	{
+		if( err ) Stop();
+		if( !Started() ) return;
+
+		std::string _str( read_buffer, size );
+
+#ifdef _DEBUG
+		std::string _data( read_buffer, size );
+		std::cout << "cl: " << _client_id << " IN: \n" << _data << std::endl;
+#endif
+
+		if( gParserPtr.get() != nullptr )
+		{
+			gParserPtr->ReceiveData( read_buffer, size );
+		}
+		Read();
+	}
+
+	void Read()
+	{
+		socket.async_receive( buffer( read_buffer ), boost::bind( &self_type::OnRead, shared_from_this(), _1, _2 ) );
+	}
+
+private:
+	ip::tcp::socket socket;
+	char read_buffer[ 1000 ];
+	bool _started;
+#ifdef _DEBUG
+	int _client_id;
+#endif
+};
+
+struct SAcceptor
+{
+	~SAcceptor() { if( m_acceptor ) delete m_acceptor; }
+	ip::tcp::acceptor* m_acceptor = nullptr;
+}tagAcceptor;
+
+
+void Acceptor( ClientConnection::ptr client, const boost::system::error_code & err )
+{
+	client->Start();
+	ClientConnection::ptr new_client = ClientConnection::Create();
+	tagAcceptor.m_acceptor->async_accept( new_client->sock(), boost::bind( Acceptor, new_client, _1 ) );
+}
+
+int main( int argc, char* argv[] )
+{
+	if( argc > 1 )
+		gPort = std::stoi( argv[ 1 ] );
+	if( argc > 2 )
+		gBulkSize = std::stoi( argv[ 2 ] );
+
+	gParserPtr = boost::shared_ptr<ParserWrapper>( new ParserWrapper( gBulkSize ) );
+	tagAcceptor.m_acceptor = new ip::tcp::acceptor( service, ip::tcp::endpoint( ip::tcp::v4(), gPort ) );
+
+	ClientConnection::ptr client = ClientConnection::Create();
+	tagAcceptor.m_acceptor->async_accept( client->sock(), boost::bind( Acceptor, client, _1 ) );
+	service.run();
+}
